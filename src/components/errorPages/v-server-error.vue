acbe88b037976a83dec8750a56856624a68091cb
Shampoony <itnewsbgn@gmail.com>
2025-04-06 15:52:04 +0300

    studetns start proj && builded

--- src/components/errorPages/v-server-error.vue
+++ src/components/errorPages/v-server-error.vue
@@ -3,7 +3,7 @@
     <main class="main">
       <section class="server-error error-sec">
         <div class="server-error__container container">
-          <img class="server-error__image" src="{% static 'images/server-error.svg' %}" alt="" />
+          <img class="server-error__image" src="" alt="" />
           <h1 class="server-error__title title">На сайте ведутся технические работы!</h1>
           <a href="https://t.me/teachandplan" target="_blank" class="server-error__button blue-btn"
             >Наш телеграм канал</a
@@ -13,3 +13,4 @@
     </main>
   </div>
 </template>
+<script setup></script>
